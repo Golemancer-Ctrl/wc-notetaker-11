5399748078623b75955e51daf41468d956b1c196
Sheev <ericmarcypro@gmail.com>
2022-12-19 21:01:44 -0500

    POST route completed, able to functionally write to the db.json file

--- Application/routes/API-routes.js
+++ Application/routes/API-routes.js
@@ -1,13 +1,37 @@
 const router = require('express').Router();
 const uuid = require('../helpers/uuid');
+// const db = require('../db/db.json');
+const fs = require('fs');
+// const path = require('path');
 
 router.post('/notes', (req, res) => {
   const {title, text} = req.body;
-  const note = {
+  const newNote = {
     //uuid will create a unique ID for each saved note
     title, text, id:uuid()
   };
-});
+  fs.readFile('db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      parsedNotes = JSON.parse(data);
+      parsedNotes.push(newNote);
+  
+      const fileWriter = fs.writeFile(
+      'db/db.json', JSON.stringify(parsedNotes),
+      (writeErr) =>
+        writeErr
+        ? console.error(writeErr)
+        : console.info('Successfully updated notes!')
+      );
+
+    // res.json(fileWriter);
+
+        }
+      }
+    )
+  }
+);
 
 
 router.delete('/notes/:id', (req, res) => {
